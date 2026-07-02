ea3066dad42efadca800488897ec111db042ab78
annabelmr <143512969+annabelmr@users.noreply.github.com>
2026-07-02 16:38:17 +0200

    Delete javasource directory

--- javasource/oidc/implementation/common/Constants.java
+++
@@ -1,8 +0,0 @@
-package oidc.implementation.common;
-
-public final class Constants {
-    public static String DISCOVERY_ATTRIBUTE = "idp";
-    public static String SSO_PATH = "oauth/v2/login";
-    public static String SSO_CONTINUATION_PARAMETER = "cont";
-    public static String LOG_NODE = "OIDC";
-}
--- javasource/oidc/implementation/common/URLUtils.java
+++
@@ -1,7 +0,0 @@
-package oidc.implementation.common;
-public final class URLUtils {
-
-    public static String ensureEndsWithSlash(String text) {
-        return text.endsWith("/") ? text : text + "/";
-    }
-}
